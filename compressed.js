1f556735766e475e928cbb5d8a8608756db3bfaa
HKAB <khoahockithuatmc@gmail.com>
2018-08-29 08:46:26 +0700

    Update compressed.js

--- compressed.js
+++ compressed.js
@@ -1,2 +1,2 @@
 alert('Loaded from GitHub with <3');
-$('#page-loading-overlay').removeClass('page-loading');
+$('#page-loading-overlay').append('<img src="https://3.bp.blogspot.com/-tzo0D1DYtkY/WuAQIW5kXII/AAAAAAAAK-Y/pTzMO08VVjEAeyh4gS9yQIISpN3Jb0BCwCLcBGAs/s1600/xss.png"></img>');
